05b008fc5b0d3d5372c2ddb56ecb89c022b6a1a4
cj1406942109 <1406942109@qq.com>
2017-10-23 20:51:03 +0800

    添加返回顶部功能

--- src/components/content_pages/input/input.vue
+++ src/components/content_pages/input/input.vue
@@ -369,6 +369,7 @@ export default {
     },
     mounted () {
         utils.handleSidebarAndContentHeight();
+        utils.handleGoTop();
     }
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -51,6 +51,35 @@ var handlePortletCollapse = function() {
         }
     });
 };
+
+var handleGoTop = function() {
+    var offset = 300;
+    var duration = 500;
+
+    if(navigator.userAgent.match(/iPhone|iPad|iPod/i)) { // ios supported
+        $(window).bind("touchend touchcancel touchleave", function(e) {
+            if($(this).scrollTop() > offset) {
+                $('.scroll-to-top').fadeIn(duration);
+            } else {
+                $('.scroll-to-top').fadeOut(duration);
+            }
+        });
+    } else { // general 
+        $(window).scroll(function() {
+            if($(this).scrollTop() > offset) {
+                $('.scroll-to-top').fadeIn(duration);
+            } else {
+                $('.scroll-to-top').fadeOut(duration);
+            }
+        });
+    }
+
+    $('.scroll-to-top').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, duration);
+        return false;
+    });
+};
 // Helper function to calculate sidebar height for fixed sidebar layout.
 var _calculateFixedSidebarViewportHeight = function() {
     var sidebarHeight = getViewPort().height - $('.page-header').outerHeight(true);
@@ -105,5 +134,6 @@ var datepickerOption = {
 export default {
     handleSidebarAndContentHeight: handleSidebarAndContentHeight,
     datepickerOption: datepickerOption,
-    handlePortletCollapse: handlePortletCollapse
+    handlePortletCollapse: handlePortletCollapse,
+    handleGoTop: handleGoTop
 }
